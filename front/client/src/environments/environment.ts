f9735319784f0622a16faae585c3f00bd7f0ddf4
Triplix1 <89905060+Triplix1@users.noreply.github.com>
2024-09-18 23:32:59 +0300

    Creating FE part of message feature

--- front/client/src/environments/environment.ts
+++ front/client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     googleAuth: 'https://accounts.google.com/o/oauth2/v2/auth',
-    baseApiUrl: 'http://localhost:5000/api'
+    baseApiUrl: 'http://localhost:5000/api',
+    hubUrl: 'hubs/'
 };
